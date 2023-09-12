9cef1cc94751117c3053cd2a9e437da5628ec3f1
StunnaDawg <jonsonallen9@gmail.com>
2023-09-12 13:41:59 -0300

    Dashboard draft 1

--- screens/ProtocolScreen.jsx
+++ screens/ProtocolScreen.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import ProtocolScreenWidget from '../components/ProtocolScreenWidget'
 
 const ProtocolScreen = () => {
   return (
     <View>
-      <Text>ProtocolScreen</Text>
+      <ProtocolScreenWidget />
     </View>
   )
 }
