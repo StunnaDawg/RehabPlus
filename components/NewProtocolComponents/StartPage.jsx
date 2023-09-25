7aef02c1975b443f4d05bcb93078334e51846ba5
StunnaDawg <jonsonallen9@gmail.com>
2023-09-25 10:44:18 -0300

    create button init

--- components/NewProtocolComponents/StartPage.jsx
+++ components/NewProtocolComponents/StartPage.jsx
@@ -1,33 +1,41 @@
-import { View, Text } from 'react-native'
-import { TextInput } from 'react-native-paper'
-import { useState } from 'react';
-import DropDownDays from './components/DropDownDays';
+import { View, Text } from "react-native"
+import { TextInput } from "react-native-paper"
+import { useState } from "react"
+import DropDownDays from "./components/DropDownDays"
 
 const StartPage = () => {
-
-    const [expanded, setExpanded] = useState(true);
-    const [text, setTitleText] = useState('')
-    const [outlineText, setOutlineText] = useState('')
-    const [weeksText, setWeeksText] = useState('')
-    const [daysPerWeek, setDaysPerWeek] = useState('1')
+  const [expanded, setExpanded] = useState(true)
+  const [text, setTitleText] = useState("")
+  const [outlineText, setOutlineText] = useState("")
+  const [weeksText, setWeeksText] = useState("")
+  const [daysPerWeek, setDaysPerWeek] = useState("1")
   return (
     <>
-    <View className='mx-4 my-1'>
+      <View className="mx-4 my-1">
         <Text>Protocol Title</Text>
-        <TextInput mode='outlined' onChangeText={text => setTitleText(text)}></TextInput>
-    </View>
+        <TextInput
+          mode="outlined"
+          onChangeText={(text) => setTitleText(text)}
+        ></TextInput>
+      </View>
 
-    <View className='mx-4 my-1'>
+      <View className="mx-4 my-1">
         <Text>Outline</Text>
-        <TextInput mode='outlined' onChangeText={text => setOutlineText(text)}></TextInput>
-    </View>
+        <TextInput
+          mode="outlined"
+          onChangeText={(text) => setOutlineText(text)}
+        ></TextInput>
+      </View>
 
-    <View className='mx-4 my-1'>
+      <View className="mx-4 my-1">
         <Text>Weeks</Text>
-        <TextInput mode='outlined' onChangeText={text => setWeeksText(text)}></TextInput>
-    </View>
+        <TextInput
+          mode="outlined"
+          onChangeText={(text) => setWeeksText(text)}
+        ></TextInput>
+      </View>
 
-<DropDownDays setTheDays={setDaysPerWeek} chosenDays={daysPerWeek}/>
+      <DropDownDays setTheDays={setDaysPerWeek} chosenDays={daysPerWeek} />
     </>
   )
 }
---
+++ components/NewProtocolComponents/components/CreateButton.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CreateButton = () => {
+  return (
+    <View>
+      <Button>Create</Button>
+    </View>
+  )
+}
+
+export default CreateButton
