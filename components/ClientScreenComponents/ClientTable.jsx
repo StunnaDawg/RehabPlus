8dd654230ec22fdb8a24a0b33d5483057f877340
StunnaDawg <jonsonallen9@gmail.com>
2023-09-28 10:49:15 -0300

    clients render on dash and client page

--- components/ClientScreenComponents/ClientTable.jsx
+++ components/ClientScreenComponents/ClientTable.jsx
@@ -1,52 +1,21 @@
 import { View, Text, Pressable } from 'react-native'
 import { DataTable, Button, IconButton } from 'react-native-paper';
-import { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import { db } from '../../firebase';
+import { collection } from 'firebase/firestore';
+import getFireStoreData from '../../functions/getFireStoreData';
 
 const ClientTable = () => {
-    
+  const [clientList, setClientList] = useState([])
+  const clientsCollectionRef = collection(db, "clients")
+  const navigation = useNavigation()
+  const isFocused = useIsFocused()
 
-    [testItems] = useState([
-        {
-            id: 1,
-            clientName: 'Harold',
-            protocol: 'ACL Phase 1',
-            active: true
-        },
-        {
-            id: 2,
-            clientName: 'Kerry',
-            protocol: 'ACL Phase 3',
-            active: true
-        },
-        {
-            id: 3,
-            clientName: 'Jerry',
-            protocol: 'ACL Phase 1',
-            active: true
-        },
-        {
-            id: 4,
-            clientName: 'Danielle',
-            protocol: 'ACL Phase 2',
-            active: true
-        },
-        {
-            id: 5,
-            clientName: 'Kenny',
-            protocol: 'none',
-            active: false
-        },
-        {
-            id: 6,
-            clientName: 'Rudy',
-            protocol: 'none',
-            active: false
-        },
-    ])
-
-const navigation = useNavigation()
-
+  useEffect(() => {
+    getFireStoreData(setClientList, clientsCollectionRef)
+    console.log("dashboard client")
+  }, [isFocused])
   return (
     <View>
        <DataTable>
@@ -56,12 +25,12 @@ const navigation = useNavigation()
         <DataTable.Title>Protocol</DataTable.Title>
         <DataTable.Title>Status</DataTable.Title>
         </DataTable.Header>
-        {testItems.map((item) => (
-        <DataTable.Row key={item.id}>
+        {clientList.map((client) => (
+        <DataTable.Row key={client.id}>
         <DataTable.Cell onPress={() => {navigation.navigate("Dashboard")}}><Button icon="account-circle" size={20}></Button></DataTable.Cell>
-        <DataTable.Cell onPress={() => navigation.navigate("Dashboard")}>{item.clientName}</DataTable.Cell>
-          <DataTable.Cell onPress={() => navigation.navigate("Protocol")}>{item.protocol}</DataTable.Cell>
-          <DataTable.Cell><IconButton icon='checkbox-blank-circle' iconColor={item.active ? 'green' : 'red'}></IconButton></DataTable.Cell>
+        <DataTable.Cell onPress={() => navigation.navigate("Dashboard")}>{client.name}</DataTable.Cell>
+          <DataTable.Cell onPress={() => navigation.navigate("Protocol")}>protocol</DataTable.Cell>
+          <DataTable.Cell><IconButton icon='checkbox-blank-circle' iconColor={client.status ? 'green' : 'red'}></IconButton></DataTable.Cell>
           {/* <DataTable.Cell><Button icon='account-eye'>View</Button></DataTable.Cell> */}
         </DataTable.Row>
       ))}
