9bcad9f4cd9bf3d67e9b814b304f5f71890973da
StunnaDawg <jonsonallen9@gmail.com>
2023-11-03 17:25:42 -0300

    loads phases

--- components/ProtocolScreenComponents/ProtocolModal.jsx
+++ components/ProtocolScreenComponents/ProtocolModal.jsx
@@ -1,12 +1,40 @@
 import { Modal } from "react-native-paper"
 
 import { View, Text } from "react-native"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { collection } from "firebase/firestore"
+import { db } from "../../firebase"
+import GetProtocolWorkouts from "../../functions/getProtocolWorkouts"
 
-const ProtocolModal = ({ protocolId }) => {
+const ProtocolModal = ({ protocolId, protocolTitle, protocolOutline }) => {
+    const [phasesData, setPhasesData] = useState([])
+    const phasesCollectionRef = collection(db, "protocols", protocolId, "phases")
+    
+    useEffect(() => {
+        const setPhases = async () => {
+        await GetProtocolWorkouts(
+            setPhasesData,
+            phasesCollectionRef,
+          )
+        }
+        setPhases()
+        console.log('phases data', phasesData)
+    }, [])
   return (
     <View>
+      <Text>ProtocolTitle: {protocolTitle}</Text>
       <Text>ProtocolModal: id {protocolId}</Text>
+      <Text>ProtocolModal: outline {protocolOutline}</Text>
+    {phasesData.map((phase) => {
+        return (
+            <View key={phase.id}>
+                <Text>Phase Title: {phase.title}</Text>
+                <Text>Phase Description: {phase.description}</Text>
+                <Text>Phase Id: {phase.id}</Text>
+            </View>
+        )
+    })
+}
     </View>
   )
 }
--- components/ProtocolScreenComponents/ProtocolScreenWidget.jsx
+++ components/ProtocolScreenComponents/ProtocolScreenWidget.jsx
@@ -1,7 +1,7 @@
 import { View, Text, Image } from "react-native"
 import { Card, Button, Portal, Modal } from "react-native-paper"
 import theImage from "../../assets/ACL-Repair-Surgery.jpg"
-import React, { createContext, useContext, useEffect, useState } from "react"
+import React, { useState } from "react"
 import { useNavigation } from "@react-navigation/native"
 import GetSingleDoc from "../../functions/getSingleDoc"
 import { collection } from "firebase/firestore"
@@ -10,7 +10,13 @@ import { useSingleProtocolContext } from "../../context/protocolContext"
 import { FIREBASE_AUTH } from "../../firebase"
 import ProtocolModal from "./ProtocolModal"
 
-const ProtocolScreenWidget = ({ protocolTitle, weeks, outline, id, userId }) => {
+const ProtocolScreenWidget = ({
+  protocolTitle,
+  weeks,
+  outline,
+  id,
+  userId,
+}) => {
   const [protocolEditData, setProtocolEditData] = useSingleProtocolContext()
   const [visible, setVisible] = useState(false)
   const showModal = () => setVisible(true)
@@ -24,64 +30,77 @@ const ProtocolScreenWidget = ({ protocolTitle, weeks, outline, id, userId }) => 
   const navigation = useNavigation()
 
   return (
-      <View>
-        <Card className="m-3">
-          <Card.Content>
-            <View className="justify-center items-center">
-              <Text variant="titleLarge" className="font-bold text-2xl">
-                {" "}
-                {protocolTitle}{" "}
-                 <Button
-                  icon="pencil"
-                  onPress={async () => {
+    <View>
+      <Card className="m-3">
+        <Card.Content>
+          <View className="justify-center items-center">
+            <Text variant="titleLarge" className="font-bold text-2xl">
+              {" "}
+              {protocolTitle}{" "}
+              <Button
+                icon="pencil"
+                onPress={async () => {
+                  await GetSingleDoc(
+                    setProtocolEditData,
+                    protocolsCollectionRef,
+                    id
+                  )
+                  navigation.navigate("EditProtocol")
+                }}
+              >
+                Edit
+              </Button>
+            </Text>
+            <View className="flex-row">
+              <Button icon="account">Assign to Client</Button>
+              <Portal>
+                <Modal
+                  visible={visible}
+                  onDismiss={hideModal}
+                  contentContainerStyle={containerStyle}
+                >
+                  <ProtocolModal
+                    protocolTitle={protocolEditData.title}
+                    protocolId={protocolEditData.id}
+                    protocolOutline={protocolEditData.description}
+                  />
+                </Modal>
+              </Portal>
+              <Button
+                icon="eye"
+                onPress={async () => {
+                  {
                     await GetSingleDoc(
                       setProtocolEditData,
                       protocolsCollectionRef,
                       id
                     )
-                    navigation.navigate("EditProtocol")
-                  }}
-                >
-                  Edit
-                </Button>
-               
-              </Text>
-                  <View className="flex-row">
-              <Button icon="account">Assign to Client</Button>
-              <Portal>
-        <Modal
-          visible={visible}
-          onDismiss={hideModal}
-          contentContainerStyle={containerStyle}
-        >
-          <ProtocolModal
-            protocolId={protocolEditData.id}
-          />
-        </Modal>
-      </Portal>
-              <Button icon='eye' onPress={async () => {
-                    {await GetSingleDoc(
-                      setProtocolEditData,
-                      protocolsCollectionRef,
-                      id
-                    ); showModal()}}}> View </Button>
+
+                    showModal()
+                  }
+                }}
+              >
+                {" "}
+                View{" "}
+              </Button>
+            </View>
+          </View>
+          <View className="flex-row">
+            <Image source={theImage} style={{ width: 100, height: 100 }} />
+            <View className="flex-1 flex-row justify-between">
+              <View className="flex-col">
+                <Text className="m-2 font-bold">Weeks: {weeks}</Text>
+                <Text className="m-2 font-bold">Description: {outline}</Text>
+                {userId !== FIREBASE_AUTH?.currentUser?.uid ? (
+                  <Text className="m-2 font-bold">Created by: {userId}</Text>
+                ) : null}
+                <Card className="m-2"></Card>
               </View>
             </View>
-            <View className="flex-row">
-              <Image source={theImage} style={{ width: 100, height: 100 }} />
-              <View className="flex-1 flex-row justify-between">
-                <View className="flex-col">
-                  <Text className="m-2 font-bold">Weeks: {weeks}</Text>
-                  <Text className="m-2 font-bold">Description: {outline}</Text>
-                  {userId !== FIREBASE_AUTH?.currentUser?.uid ? <Text className="m-2 font-bold">Created by: {userId}</Text> : null}
-                  <Card className="m-2">
-                  </Card>
-                </View>
-              </View>
-            </View>
-          </Card.Content>
-        </Card>
-      </View>
+          </View>
+        </Card.Content>
+      </Card>
+    </View>
   )
 }
 
