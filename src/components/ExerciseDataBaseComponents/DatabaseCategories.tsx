ca50481fa3d8ba28c1abdd9e511ea0baec3b5e5b
StunnaDawg <jonsonallen9@gmail.com>
2023-11-24 22:34:05 -0400

    No type errors but no exercises dispal;yed

--- src/components/ExerciseDataBaseComponents/DatabaseCategories.tsx
+++ src/components/ExerciseDataBaseComponents/DatabaseCategories.tsx
@@ -14,6 +14,7 @@ import getExerciseCategoryData from "../../functions/getExerciseCategoriesData"
 const DatabaseCategories = () => {
   const [exerciseCategories, setExerciseCategories] = useState<ExerciseDataBaseCategory[]>([])
   const [exercisesDisplayed, setExercisesDisplayed] = useState<ExerciseDataBaseExercise[]>([])
+  const [currentExercises, setCurrentExercises] = useState<ArrayLike<ExerciseDataBaseExercise>>([])
   const [pressedButtonId, setPressedButtonId] = useState('85ZJ5LvyxECGoN0GMjHZ')
   const exercisesCollectionRef = collection(db, "exerciseCategories")
   const isFocused = useIsFocused()
@@ -38,6 +39,26 @@ const DatabaseCategories = () => {
     console.log("display data", exercisesDisplayed)
   }, [exercisesDisplayed])
 
+  useEffect(() => {
+    console.log("display data", currentExercises)
+  }, [currentExercises])
+
+  useEffect(() => {
+
+  }, [currentExercises])
+
+  const getExercisesForCategory = (categoryId: string) => {
+    const category = exerciseCategories.find(cat => cat.id === categoryId);
+    
+    if (category) {
+      console.log('category:',category)
+
+      return category
+    } else {
+      return ['']
+    }
+  };
+
   const renderItem: ListRenderItem<ExerciseDataBaseCategory> = ({item}) => (
     <Button
       key={item.id}
@@ -49,23 +70,12 @@ const DatabaseCategories = () => {
           : setPressedButtonId(item.id)};
           console.log(item.id)
           getExerciseCategoryData(setExercisesDisplayed, collection(db, 'exercsiseCategories', item.id, 'exercises'))
+          getExercisesForCategory(pressedButtonId)
       }}
     >
       {item.title}
     </Button>
   )
-
-  const getExercisesForCategory = (categoryId: string) => {
-    const category = exerciseCategories.find(cat => cat.id === categoryId);
-    
-    if (category) {
-      console.log(category)
-
-      return category
-    } else {
-      return ['']
-    }
-  };
 
   return (
     <>
@@ -77,13 +87,13 @@ const DatabaseCategories = () => {
         showsHorizontalScrollIndicator={false}
       />
      <FlatList className='pb-96'
-  data={getExercisesForCategory(pressedButtonId)}
+  data={currentExercises}
   keyExtractor={(item: ExerciseDataBaseExercise) => item.id}
   renderItem={({ item }) => {
     const exerciseNameKey = Object.keys(item).find(key => key !== "id");
 
     if(exerciseNameKey) {
-    const exerciseTitle = item.exercise.title;
+    const exerciseTitle = item.exercise?.title;
 
 
     return (
