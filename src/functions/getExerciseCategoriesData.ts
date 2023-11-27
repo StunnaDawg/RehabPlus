87ff3d2636ec993b214290ee68e3dc9ccfbf95b0
StunnaDawg <jonsonallen9@gmail.com>
2023-11-26 21:00:45 -0400

    realized i made a spelling mistake

--- src/functions/getExerciseCategoriesData.ts
+++ src/functions/getExerciseCategoriesData.ts
@@ -1,4 +1,4 @@
-import { getDocs, CollectionReference, collection } from "firebase/firestore"
+import { getDocs, CollectionReference, collection, doc } from "firebase/firestore"
 import { Dispatch, SetStateAction } from "react";
 import { ExerciseDataBaseExercise } from "../@types/firestore";
 import { db } from "../firebase";
@@ -6,17 +6,17 @@ import { db } from "../firebase";
 const getExerciseCategoryData = async (setCategoriesState: Dispatch<SetStateAction<ExerciseDataBaseExercise[]>>, categoriesCollectionId: string) => {
     try {
         console.log('Collection id', categoriesCollectionId)
-        const categoriesCollection = collection(db, 'exercsiseCategories', categoriesCollectionId, 'exercises')
+        const category = doc(db, 'exerciseCategories', categoriesCollectionId)
+        const categoriesCollection = collection(category, 'exercises')
         const data = await getDocs(categoriesCollection)
     
         const filteredData = data.docs.map((doc) => ({
           ...doc.data(),
           id: doc.id,
-          exercise: doc.data().exercises
-        //   exercise: {
-        //     title: doc.data().title,
-        //     description: doc.data().description
-        //   }
+          exercise: {
+            title: doc.data().title,
+            description: doc.data().description
+          }
         }))
         console.log("filtered data get Category Exercises", filteredData)
         setCategoriesState(filteredData)
