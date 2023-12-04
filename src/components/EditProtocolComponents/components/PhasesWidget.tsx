ef5d7d2fdf38837f1ec56b77ca210f4195d1d1a6
StunnaDawg <jonsonallen9@gmail.com>
2023-12-04 13:45:12 -0400

    few changes

--- src/components/EditProtocolComponents/components/PhasesWidget.tsx
+++ src/components/EditProtocolComponents/components/PhasesWidget.tsx
@@ -4,23 +4,29 @@ import { useNavigation } from "@react-navigation/native"
 import { useCurrentPhasesIdContext } from "../../../context/phasesIdContext"
 import { NavigationType } from "../../../@types/navigation"
 
-type PhasesWidgetProps = { 
+type PhasesWidgetProps = {
   phasesTitle: string
   phaseId: string
 }
 
-const PhasesWidget = ({ phasesTitle, phaseId
-}: PhasesWidgetProps) => {
-  const { setCurrentPhasesId} = useCurrentPhasesIdContext()
+const PhasesWidget = ({ phasesTitle, phaseId }: PhasesWidgetProps) => {
+  const { setCurrentPhasesId } = useCurrentPhasesIdContext()
   const navigation = useNavigation<NavigationType>()
 
   return (
     <Card mode="contained" className="mt-3 mx-14 ">
       <Card.Content className="flex-1 flex-row justify-center items-center">
         <Text variant="titleLarge">{phasesTitle}</Text>
-      <Card.Actions className="flex-1 flex-row justify-center items-center">
-        <Button onPress={() => {setCurrentPhasesId(phaseId); navigation.navigate('EditProtocolWorkoutScreen')}}>Edit Phase</Button>
-      </Card.Actions>
+        <Card.Actions className="flex-1 flex-row justify-center items-center">
+          <Button
+            onPress={() => {
+              setCurrentPhasesId(phaseId)
+              navigation.navigate("AddProtocolWorkoutScreen")
+            }}
+          >
+            Edit Phase
+          </Button>
+        </Card.Actions>
       </Card.Content>
     </Card>
   )
