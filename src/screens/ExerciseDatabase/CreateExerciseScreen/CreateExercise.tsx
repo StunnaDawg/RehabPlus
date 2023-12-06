b15bfb56a30323d1b13ab83d60062da26c9ed31c
StunnaDawg <jonsonallen9@gmail.com>
2023-12-06 19:09:49 -0400

    Create exercise page initialized

---
+++ src/screens/ExerciseDatabase/CreateExerciseScreen/CreateExercise.tsx
@@ -0,0 +1,11 @@
+import { Text, View, ScrollView } from "react-native"
+
+const CreateExercise = () => {
+  return (
+    <ScrollView>
+      <Text className="text-xl">Create an Exercise</Text>
+    </ScrollView>
+  )
+}
+
+export default CreateExercise
