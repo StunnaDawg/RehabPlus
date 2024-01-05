2ed71840d15b60c16a6ced43ca9433b10166eb98
StunnaDawg <jonsonallen9@gmail.com>
2024-01-04 22:06:24 -0400

    upload still stuck, but progress

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -1,8 +1,9 @@
 import { View, Text, Image } from "react-native"
 import * as ImagePicker from "expo-image-picker"
 import React, { Dispatch, SetStateAction, useState } from "react"
-import { storage } from "../firebase"
+import * as FileSystem from "expo-file-system"
 import { Button } from "react-native-paper"
+import { getStorage, ref, uploadString } from "firebase/storage"
 
 type UrploadImageType = {
   setUri: Dispatch<SetStateAction<string>>
@@ -10,6 +11,7 @@ type UrploadImageType = {
 
 const UploadImage = ({ setUri }: UrploadImageType) => {
   const [image, setImage] = useState<string | null>()
+  const [uploading, setUploading] = useState<boolean>(false)
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -26,12 +28,52 @@ const UploadImage = ({ setUri }: UrploadImageType) => {
     }
   }
 
+  const uploadMedia = async () => {
+    setUploading(true)
+
+    try {
+      if (image) {
+        const { uri } = await FileSystem.getInfoAsync(image)
+
+        const blob: string = await new Promise((resolve, reject) => {
+          const xhr = new XMLHttpRequest()
+          xhr.onload = () => {
+            resolve(xhr.response)
+          }
+          xhr.onerror = (e) => {
+            reject(new TypeError("Network Request Failed"))
+            console.error(e)
+          }
+          xhr.responseType = "blob"
+          xhr.open("GET", uri, true)
+          xhr.send(null)
+        })
+
+        const fileName = image.substring(image.lastIndexOf("/") + 1)
+        const storage = getStorage()
+        const storageRef = ref(storage, fileName)
+
+        await uploadString(storageRef, blob, "base64", {
+          contentType: "image/jpeg",
+        })
+
+        console.log("imageUploaded")
+        setImage("")
+      }
+    } catch (err) {
+      console.error(err)
+    } finally {
+      setUploading(false)
+    }
+  }
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       {image && (
         <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
       )}
       <Button onPress={pickImage}>Pick an image from camera roll</Button>
+      <Button onPress={uploadMedia}>Upload Image</Button>
     </View>
   )
 }
